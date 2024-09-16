53261e9530a7f4cbb66d473a923f224031045c48
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2024-09-17 01:57:54 +0530

    update not-found ui

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-bodyColor">
-      <h1 className="text-4xl font-bold text-gray-800">404 - Page Not Found</h1>
-      <p className="mt-4 text-lg text-gray-600">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-bodyColor bg-opacity-20">
+      <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
+      <p className="mt-4 text-lg text-gray-100">
         Oops! The page you are looking for does not exist.
       </p>
       <Link
